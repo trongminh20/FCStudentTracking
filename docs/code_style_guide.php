70ed8263c7f40cc3d0e44d83619e3dfcccd67322
Minh Phan <trongminh20@gmail.com>
2021-01-27 00:26:48 -0800

    adding coding style guide

---
+++ docs/code_style_guide.php
@@ -0,0 +1,49 @@
+<?php
+// an constant should be declared with full capital letters and _
+define("CONSTANT_NAME","constant value");
+
+// all indentations in code should be converted to spaces
+
+// function should have comment about what it does in PHPDoc style above the function declaration.
+
+// naming php files:
+// if it is a view:
+//      v_name_of_view.php
+//if it is a controller:
+//      c_name_of_controller.php
+//if it is a model:
+//      m_name_of_model.php
+
+//** ALWAYS PULL CODE BEFORE START CODING.**
+
+//first letter of class name must be a capital letter
+class ClassName {
+    // first letter of variable should be a lower letter
+    private $variableName;
+
+    /**
+     *What it does
+     * parameter
+     * return type.
+     */
+    function do_sth_function_name(){
+    }
+}
+
+?>
+<!DOCYPE html>
+<html>
+    <head>
+<!--        the tag below is for auto refresh page every 2 seconds. this is my suggestion for html/css testing. if
+you had another choice, just ignore it-->
+        <meta http-equiv="refresh" content="2">
+    </head>
+    <body>
+<!--    a div should have id and class -->
+        <div class="class_name" id="nameOfId"></div>
+<!--        the alt contribute of img tag should not be empty -->
+        <img src="#" alt="should have content">
+    </body>
+</html>
+
+
