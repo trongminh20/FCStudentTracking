de57686770990048f9d3a847169ae14ad3a4e5d4
Minh Phan <trongminh20@gmail.com>
2021-02-02 01:48:33 -0800

    add classes User and Model

--- Models/User.php
+++ Models/User.php
@@ -1,31 +1,47 @@
 <?php
-class User extends Model{
-
+class User{
     private $username;
     private $password;
     private $email;
     private $phoneNumber;
 
-    public function __construct($username, $password, $email="", $phoneNumber=""){
-//        parent::__construct();
+    public function __construct($username, $password, $email= "", $phoneNumber=""){
         $this->username = $username;
-        $this->password = SHA1($password);
+        $this->password = $password;
         $this->email = $email;
-        $this->phoneNumber= $phoneNumber;
-
+        $this->phoneNumber = $phoneNumber;
     }
 
-    function get_username(){}
+    function get_username(){
+        return $this->username;
+    }
 
-    function change_password(){}
+    function change_password($password){
+        $this->password = $password;
+    }
 
-    function get_email(){}
+    function get_email(){
+        return $this->email;
+    }
+    function change_email($email){
+        $this->email = $email;
+    }
 
-    function get_phone_number(){}
+    function get_phone_number(){
+        return $this->phoneNumber;
+    }
 
-    function update_email(){}
+    function change_phone_number($pNumber){
+        $this->phoneNumber = $pNumber;
+    }
 
-    function update_phone_number(){}
+    function toArray(){
+        return [
+            'username'=>$this->username,
+            'password'=> $this->password,
+            'email' => $this->email,
+            'phone_number'=> $this->phoneNumber
+        ];
+    }
 
-    
 }
