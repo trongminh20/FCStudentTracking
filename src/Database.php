5d8ec0e33e4e807eb5ca44ff03d687170f6456ec
Minh Phan <trongminh20@gmail.com>
2021-02-04 08:38:32 -0800

    mp working on class Database

--- src/Database.php
+++ src/Database.php
@@ -5,7 +5,8 @@ class Database
 {
     private $pdo;
 
-    /**Create new connection to Database by using PDO
+    /**
+     * Create new connection to Database by using PDO
      * Database constructor.
      * @param PDO $pdo
      */
@@ -16,7 +17,7 @@ class Database
     /**
      * getting all records that existing in one table
      * @param $table
-     * @return mixed array
+     * @return false|array
      */
     public function get_records($table){
         $query = "SELECT * FROM ".$table;
@@ -35,11 +36,57 @@ class Database
         $query = "SELECT * FROM $table WHERE id = ?";
         $stm = $this->db->prepare($query);
         $stm ->execute([$id]);
-        return $stm ->fetch(PDO::FETCH_ASSOC);
+        return $stm->fetch(PDO::FETCH_ASSOC);
     }
 
-    function create_records($table, $data){
+    /**
+     * Insert one record into existing table in database
+     * @param $table
+     * @param $data as an Array that we got from toArray()
+     * @return false|string
+     */
+    function create_record($table, $data){
+        $cols = array_keys($data);
+        $colInTable = implode(',', $cols);
+        $binding = ":".implode(',:', $cols);
 
+        $query = "INSERT INTO $table($colInTable) VALUES ($binding)";
+
+        $stm = $this->pdo->prepare($query);
+
+        $stm = $this->bind_data($stm, $data);
+
+        $status = $stm ->execute();
+        // Returns the ID of the last inserted row or sequence value
+        return ($status) ? $this->pdo->lastInsertId() : false;
     }
 
+    /**
+     * delete one specific row from existing table
+     * @param $table
+     * @param $id
+     * @return void
+     */
+    function delete($table, $id){
+        $query = "DELETE FROM ".$table." WHERE id = ?";
+        $stm = $this->pdo->prepare($query);
+        try{
+            $stm -> execute([$id]);
+        }catch(PDOException $e){
+            $e->getMessage();
+        }
+    }
+    /**
+     * binding data -> sql query
+     * @param $sql
+     * @param $data
+     * @return mixed
+     */
+    private function bind_data($sql , $data){
+        $stm = $sql;
+        foreach($data as $k => $v){
+            $stm->bindValue(':'.$k, $v);
+        }
+        return $stm;
+    }
 }
