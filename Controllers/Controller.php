419bfa5289016335caa12678327a00f825cc1d0a
Minh Phan <trongminh20@gmail.com>
2021-02-04 13:24:34 -0800

    finished index.php, Model.php

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -6,14 +6,17 @@ class Controller{
     }
 
     public function index($dr){
-        if($dr[0] == 'v'){
-            return "Views/".$dr.".php";
-        } else if($dr[0] == 'c'){
-            return "Controllers/".$dr.".php";
-        }else if($dr[0] == 'm'){
-            return "Models/".$dr.".php";
+        $action = filter_input(INPUT_POST,$dr, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if($action[0] == 'v'){
+            return "Views/".$action.".php";
+        } else if($action[0] == 'c'){
+            return "Controllers/".$action.".php";
+        }else if($action[0] == 'm'){
+            return "Models/".$action.".php";
         }else{
             return "404.php";
         }
     }
+
 }
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['action'])){
+    $action = $_POST['action'];
+}else if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}else{
+    $action = 'v_home';
+}
+//getting const information
+$conf = require "src/config.php";
+
+//Coonect to database hosting
+$dbConnect = "mysql:host=".$conf['db_host'].";dbname=".$conf['db_name'].";charset=".$conf['db_charset'];
+$dbUsername = $conf['db_username'];
+$dbPassword = $conf['db_password'];
+$pdo = new PDO($dbConnect, $dbUsername, $dbPassword,$conf['db_options']);
+$database = new Database($pdo);
+
+//new Controller for control flow
+$controller = new Controller($database);
+
+//Routing to destination page
+require $controller -> index($action);
