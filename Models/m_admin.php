4b5254201e54d71ab7f6dd9ace2b4a4a53ebced5
Minh Phan <trongminh20@gmail.com>
2021-02-04 20:19:15 -0800

    edit sql file

--- Models/m_admin.php
+++ Models/m_admin.php
@@ -1,12 +1,13 @@
 <?php
 class Admin extends User{
+    private $department;
 
-    public function __construct($username, $password, $email, $phoneNumber){
+    public function __construct($username, $password, $email, $phoneNumber, $department){
         parent::__construct($username, $password, $email, $phoneNumber);
+        $this->department = $department;
     }
 
     public function create_account (){
-
     }
 
     public function delete_account(){}
