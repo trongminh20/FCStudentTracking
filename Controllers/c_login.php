723dad970a97117c0a550fcddeb8fc37774d2385
Minh Phan <trongminh20@gmail.com>
2021-02-08 14:28:56 -0800

    On branch minhphan modified:   Controllers/c_login.php new file:   Views/v_header.php modified:   Views/v_login.php new file:   css/customize.css new file:   js/customize.js deleted:    login_form_template.html

--- Controllers/c_login.php
+++ Controllers/c_login.php
@@ -3,9 +3,11 @@ if(isset($_POST['submit'])) {
     $username = $_POST['username'];
 
     if ($username = 'minh') {
+
         $_SESSION['username'] = $username;
         $success = $controller -> sign_in($username, 'ronminh');
-        echo $success;
+
+        header ("Location:?action=v_home");
     } else {
         header("location:?action=v_login");
     }
---
+++ Views/v_header.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/customize.css">
+        <script src="../js/customize.js"></script>
+    </head>
+
