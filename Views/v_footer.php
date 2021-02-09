551fb2d4363ceed85dc41334f260c868b79b60ac
Minh Phan <trongminh20@gmail.com>
2021-02-09 12:26:38 -0800

    On branch minhphan 	modified:   Views/v_footer.php 	modified:   Views/v_header.php

--- Views/v_footer.php
+++ Views/v_footer.php
@@ -1,9 +1,7 @@
 <?php
-<<<<<<< HEAD
 ?>
 <!--Footer Section-->
 <div class="footer">
   <p>© 2021 First College. All Rights Reserved.</p>
 </div>
-=======
->>>>>>> 405abcfe8091b64ef023da9cb151bc29829020fb
+
