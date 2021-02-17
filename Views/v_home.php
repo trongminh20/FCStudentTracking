958e265bbf2c1a849aed72cc4184152f1d9bd4d4
Minh Phan <trongminh20@gmail.com>
2021-02-16 19:04:09 -0800

    On branch minhphan  Changes to be committed: 	modified:   Views/v_home.php

--- Views/v_home.php
+++ Views/v_home.php
@@ -6,7 +6,12 @@ include "v_masterPage_sidebar.php";
 //?>
 <div id="mainContent">
     <?php
-        $data = $model->select_data('Employees');
-        $view->display_as_table("", "", $data);
+        echo "<h3>Employees table</h3>";
+        $data1 = $model->select_data('Employees');
+        $view->display_as_table("", "", $data1);
+
+        echo "<h3>Fees table</h3>";
+        $data2 = $model -> select_data('fees');
+        $view -> display_as_table("","", $data2);
     ?>
 </div>
