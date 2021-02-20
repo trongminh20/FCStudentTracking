f58b97d1cc48afb8eef0fd7af492d003757cf9c2
Minh Phan <trongminh20@gmail.com>
2021-02-20 02:51:44 -0800

    minhphan finished change password feature  Changes to be committed: 	modified:   Controllers/c_change_pass.php

--- Controllers/c_change_pass.php
+++ Controllers/c_change_pass.php
@@ -1,20 +1,19 @@
 <?php
-//print_r ($_SESSION['user']);
- $id = $_SESSION['user']['id'];
- $new_pass = 'trongminh';
- $table = 'Employees';
-$_SESSION['user']['password'] = $new_pass;
- $data = $_SESSION['user'];
-
- print_r($data);
- if(!empty($new_pass)){
- 	$model ->change_password($table, $data, $id);
- 	$_SESSION['change_password_announce'] = 'Your password has been changed';
- }else{
-     $_SESSION['change_password_announce'] = 'Your password has been changed';
- }
- header("Location:?action=v_change_password");
- exit();
-
+print_r($_POST);
+if(isset($_POST['change_pass'])) {
+    $new = $_POST['new_pass'];
+    $_SESSION['user']['password'] = $new ;
+    $id = $_SESSION['user']['id'];
+    $table = 'Employees';
+    $data = $_SESSION['user'];
+    if (!empty($new)) {
+        $model->change_password($table, $data, $id);
+        $_SESSION['change_password_announce'] = 'Your password has been changed';
+    } else {
+        $_SESSION['change_password_announce'] = 'Your password has been changed';
+    }
+    header("Location:?action=v_change_password");
+    exit();
+}
 
 
