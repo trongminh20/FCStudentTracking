1522cf653d6effa7ec0a4efca1db5784370c18ae
Minh Phan <trongminh20@gmail.com>
2021-02-24 11:03:33 -0800

    On branch demo  Changes to be committed: 	modified:   Views/v_generate_invoice.php 	modified:   Views/v_invoice.php

--- Views/v_generate_invoice.php
+++ Views/v_generate_invoice.php
@@ -1,2 +1,24 @@
 <?php
-print_r($_POST);
+$data = $_POST;
+print_r($data);
+?>
+
+<table>
+    <thead>
+        <tr>
+            <?php
+                foreach($data as $key => $val){
+                    echo "<td>$key</td>";
+                    echo"<tr>";
+                    foreach($val as $k => $v){
+
+                        echo "<td>$v</td>";
+
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </tr>
+    </thead>
+
+</table>
--- Views/v_invoice.php
+++ Views/v_invoice.php
@@ -16,30 +16,27 @@ include "v_masterPage_sidebar.php";
             </tr>
             </thead>
             <tbody>
-            <tr>
-            <td>
-                <input type="button" id="addMoreRow" onclick="add_fields();" value="Add more row">
-            </td>
-            </tr>
-            <?php
-                    $content= array();
-                    foreach($content as $row):?>
+
             <tr>
                 <td>
-                    <input id='' class='' type='number' name='quantity[]' value='<?=$row['quantity']?>'>
+                    <input id='' class='' type='number' name='quantity[]' value=''>
                 </td>
                 <td>
-                    <input id='' class='' type='text' name='description[]' value='<?=$row['desciprtion']?>'>
+                    <input id='' class='' type='text' name='description[]' value=''>
                 </td>
                 <td>
-                    <input id='' class='' type='number' name='unitPrice[]' value='<?=$row['unitprice']?>'>
+                    <input id='' class='' type='number' name='unitPrice[]' value=''>
                 </td>
                 <td>
-                    <input id='' class='' type='number' name='total[]' value='<?=$row['total']?>'>
+                    <input id='' class='' type='number' name='total[]' value=''>
                 </td>
             </tr>
-            <?php endforeach;?>
             </tbody>
+            <tr>
+                <td>
+                    <input type="button" id="addMoreRow" onclick="add_fields();" value="Add more row">
+                </td>
+            </tr>
             <tfoot>
             <tr>
                 <td></td>
@@ -54,11 +51,11 @@ include "v_masterPage_sidebar.php";
                 <td></td>
                 <td>TOTAL FEES</td>
                 <td>
-                    <input id="" class="" type="number" name="quantity" value="">
+                    <input id="" class="" type="number" name="totalFees" value="">
                 </td>
             </tr>
-
             </tfoot>
+
         </table>
         <input id="" class="btn btn-primary" type="submit" name="submit" value="Generate">
     </form>
@@ -66,10 +63,12 @@ include "v_masterPage_sidebar.php";
 <script>
     function add_fields() {
         var loc = document.getElementById("invoice_table").getElementsByTagName('tbody')[0].insertRow(-1).innerHTML = "<tr>" +
-            "<td><input id='' class='' type='number' name='quantity[]' value='"+<?=$row['quantity'] ?>+"'></td>" +
-            "<td><input id='' class='' type='text' name='description[]' value='"+<?=$row['desciprtion']?>+"'></td>" +
-            "<td><input id='' class='' type='number' name='unitPrice[]' value='"+<?=$row['unitprice']?>+"'></td>" +
-            "<td><input id='' class='' type='number' name='total[]' value='"+<?=$row['total']?>+"'></td>" + "</tr>";
+            "<td><input id='' class='' type='number' name='quantity[]' value=''></td>" +
+            "<td><input id='' class='' type='text' name='description[]' value=''></td>" +
+            "<td><input id='' class='' type='number' name='unitPrice[]' value=''></td>" +
+            "<td><input id='' class='' type='number' name='total[]' value=''></td>" + "</tr>";
     }
+
+
 
 </script>
