982b34ef9e714576032316ad938d46aeadebbc91
Minh Phan <trongminh20@gmail.com>
2021-02-25 13:37:34 -0800

    On branch demo  Changes to be committed: 	modified:   src/config.php

--- src/config.php
+++ src/config.php
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<?php
-return [
-    'db_host' => 'localhost',
-    'db_username' => 'root',
-    'db_password' => 'letmein',
-    'db_name' => 'fc_student_tracking',
-    'db_charset' => 'utf8',
-    'db_options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false]
-];
-=======
 <?php
 return ['DATABASE' => [
     'db_host' => 'localhost',
@@ -30,4 +17,4 @@ return ['DATABASE' => [
         'logo' => 'images/logo.png'
         ]
 ];
->>>>>>> origin/demo
+
