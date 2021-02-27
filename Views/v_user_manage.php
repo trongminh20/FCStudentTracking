0505fa5ad510ada5b014fdb1def298bbcbebc4af
Minh Phan <trongminh20@gmail.com>
2021-02-27 10:56:32 -0800

    On branch Jessie  Changes to be committed: 	modified:   Views/v_user_manage.php

--- Views/v_user_manage.php
+++ Views/v_user_manage.php
@@ -6,22 +6,35 @@ include "v_masterPage_sidebar.php";
 ?>
 <div id="mainContent">
     <h1>USER MANAGEMENT INTERFACE</h1>
+
     <?php
+    //display error or announcements if existed
     if (!empty($_SESSION['manage_info'])) {
         echo "<h4>" . $_SESSION['manage_info'] . "</h4>";
         unset($_SESSION['manage_info']);
     }
     //the cols that are not displayed
-
     $unsetCols = ['Password'];
-
     $loadTable = $model->select_displayed_data("employees",$unsetCols);
+    $id = "";
+    $class = "table";
+    $actionForEditFunc = "v_edit_user";
+    $actionForDelFunc = "c_delete_uer";
+    $actionForResetPass = "c_reset_pass";
 
-    $view->show_table_with_manage_functions("", "table", "v_edit_user", "c_delete_user", "c_reset_pass", $loadTable);
+    $view->show_table_with_manage_functions($id, $class, $actionForEditFunc, $actionForDelFunc, $actionForResetPass, $loadTable);
+
     ?>
 
     <?php
     $addInfo = $model->get_type("employees");
-    $view->display_form("formAddUser", " form form_add_user", "Adding new Employee", "c_add_user", "POST", 'employees',$model);
+    $id='formAddUser';
+    $class= "form form_add_user";
+    $fieldSet = "Add new Employees";
+    $action = 'c_add_user';
+    $method = 'POST';
+    $table = 'employees';
+
+    $view->display_form($id, $class, $fieldSet,$action,$method, $table,$model);
     ?>
 </div>
