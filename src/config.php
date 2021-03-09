44a5c941c9ada108442f6378d424a21c5e3ee1a2
athomas97 <allythomas97@gmail.com>
2021-03-09 12:16:25 -0800

    On branch Ally  	modified:   src/config.php

--- src/config.php
+++ src/config.php
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 <?php
-return [
+return ['DATABASE' => [
     'db_host' => 'localhost',
     'db_username' => 'root',
     'db_password' => 'letmein',
@@ -9,25 +8,13 @@ return [
     'db_options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false]
-];
-=======
-<?php
-return ['DATABASE' => [
-    'db_host' => 'localhost',
-    'db_username' => 'root',
-    'db_password' => 'password',
-    'db_name' => 'fc_student_tracking',
-    'db_charset' => 'utf8',
-    'db_options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false]
 ],
     'INVOICE_INFO' => ['name' => 'First College',
-        'address' => '532 Leon Ave, Second Floor, Kelowna, BC V1Y 6J6',
+        'address' => '532 Leon Ave, Second Floor',
+        'city'=> 'Kelowna, BC V1Y 6J6',
         'tel' => '778-478-6611',
         'fax' => '778-478-6610',
-        'Email' => 'enrollment@firstcollege.ca',
+        'email' => 'enrollment@firstcollege.ca',
         'logo' => 'images/logo.png'
         ]
 ];
->>>>>>> origin/demo
