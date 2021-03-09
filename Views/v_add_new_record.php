10002f0572daa2399af7cc0a6ad3ac6d1a322ce6
Jessie Zhang <wzhang0529@gmail.com>
2021-03-09 13:04:54 -0800

    On branch Jessie 	modified:   Views/v_add_new_record.php

--- Views/v_add_new_record.php
+++ Views/v_add_new_record.php
@@ -5,8 +5,32 @@ include "v_masterPage_header.php";
 include "v_masterPage_sidebar.php";
 ?>
 
-<div id="mainContent">
-    <h4>add new records</h4>
+<div class="container" id="mainContent">
+    <h4>Add New Student Records</h4>
+    <div class="row">
+        <div class="col-xs-12">
+    		<form class="col-lg-8" action="" method="POST">
+				<div class="form-group" style="padding-top: 20px">
+            		<label for="selectSection">Select Section to Insert:</label>
+            		<select class="form-control" name="select_Section" id="selectSection">
+                		<option value="Admission Prior to Start Date">Admission Prior to Start Date</option>
+                		<option value="Prior to Practice Education">Prior to Practice Education</option>
+                		<option value="Graduation">Graduation</option>
+            		</select>
+            		<div class="form-group" style="padding-top: 20px; padding-bottom: 10px">
+                		<input type="submit" name='add_record' value='ADD' class="btn
+              		btn-primary">
+            		</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+
+?>
+
     <a href="?action=v_priorToPracticeEducation_form">Add practice to education </a><br>
     <a href="?action=v_admPriorToStartDate_form">Add admission Prior to Start Date </a><br>
     <a href="?action=v_graduation_form">Add graduation </a><br>
