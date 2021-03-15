0765bfc4e5ef3ceed89f2fdd8eac658456db2f63
Minh Phan <trongminh20@gmail.com>
2021-03-15 02:59:03 -0700

    On branch minhphan  Changes to be committed: 	modified:   js/javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,8 @@
 /*choose between payment methods*/
-// function check() {
-//     var elem = document.getElementById("payOptions");
-//     if (elem.value == 'Others') {
-//         var att = document.createAttribute("name");
-//         att.value= "pay_option";
-//         document.getElementById('otherPayOptions').setAttributeNode(att);
-//         document.getElementById('otherPayOptions').style.display = "block";
-//
-//     } else {
-//         document.getElementById("otherPayOptions").style.display = 'none';
-//     }
-// }
+
 function check(){
         let e = document.getElementById("payOptions");
         let name = document.createAttribute("name");
-
         if(e.value == 'Others'){
             name.value = "pay_option";
             document.getElementById("otherOptionDiv").setAttribute("style","display=block;")
