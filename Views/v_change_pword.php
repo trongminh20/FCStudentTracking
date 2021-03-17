15bb3f087d6521f17eb8a0d9281a0861c1a1b714
Admin <alexdolor27@gmail.com>
2021-03-16 21:19:46 -0700

    manual created

---
+++ Views/v_change_pword.php
@@ -0,0 +1,39 @@
+<?php
+include "v_tech_nav.php";
+?>
+<div id="main-container">
+    <div class="content" id="item1">
+        <h1>CHANGE PASSWORD</h1>
+        <br>
+        <p>This function allows user to change his or her password</p>
+    </div>
+    <div class="content" id="item2">
+        <h1>PROCEDURE</h1>
+        <br><br>
+        <p>1. Press user icon <img src="user.png" alt="user_icon"> on the upper right-hand corner of the website.</p>
+        <br>
+        <p>2. Select Change Password.</p>
+        <br>
+        <img src="change_password.png" alt="change">
+        <br>
+        <p>3. Enter new password in the "Enter New Password" box.</p>
+        <br>
+        <img src="new_pword.png" alt="new_password">
+        <br>
+        <p>4. Press enter <img src="enter.jpg" alt="enter" style="width:80px;height:40px;"> or select change button <img src="change.png" alt="change" 
+              style="width:80px;height:40px;">.</p>
+        <br>
+        <p>5. Message "Your password has been changed" will appear confirming change password was successful.</p>
+    </div>
+    
+
+
+    <?php
+    include "v_tech_footer.php";
+    ?>
+</div>
+
+</div><!--end of wrapper-->
+
+
+
---
+++ Views/v_search_man.php
@@ -0,0 +1,37 @@
+<?php
+include "v_tech_nav.php";
+
+?>
+<div id="main-container">
+    <div class="content" id="item1">
+        <h1>SEARCH</h1> 
+        <br><br>      
+        <p> This function allows user to search for student record by entering student ID or student name.
+        </p>
+    </div>
+    <div class="content" id="item2">
+        <h1> PROCEDURE </h1>
+        <br>
+        <p> 1. Go to search box.</p>
+        <br>        
+        <p><img src="search.png" alt="search_box"></p>       
+        <br>    
+        <p> 2. Enter the student ID or student name.</p>
+        <br>
+        <p>(Note: If using student ID, type # followed by student number. If using student name, type @ followed by student name.)</p>
+        <br><br>
+        <p> 3. Press Enter  <img src="enter.jpg" alt="enter" style="width:80px;height:40px;"> or search tab button <img src="search_button.jpg" style="width:80px;height:40px;"></p> 
+        <br>
+    </div>
+  
+
+
+    <?php
+    include "v_tech_footer.php";
+    ?>
+</div>
+
+</div><!--end of wrapper-->
+
+
+
