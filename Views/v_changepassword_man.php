6471b774ab110f7856968ab17e094103e3389354
Admin <alexdolor27@gmail.com>
2021-03-17 13:47:28 -0700

    updated

--- Views/v_changepassword_man.php
+++ Views/v_changepassword_man.php
@@ -11,17 +11,17 @@ include "v_tech_nav.php";
     <div class="content" id="item2">
         <h1>PROCEDURE</h1>
         <br><br>
-        <p>1. Press user icon <img src="user.png" alt="user_icon"> on the upper right-hand corner of the website.</p>
+        <p>1. Press user icon <img src="images/user.png" alt="user_icon"> on the upper right-hand corner of the website.</p>
         <br>
         <p>2. Select Change Password.</p>
         <br>
-        <img src="change_password.png" alt="change">
+        <img src="images/change_password.png" alt="change">
         <br>
         <p>3. Enter new password in the "Enter New Password" box.</p>
         <br>
-        <img src="new_pword.png" alt="new_password">
+        <img src="images/new_pword.png" alt="new_password">
         <br><br>
-        <p>4. Press enter <img src="enter.jpg" alt="enter" style="width:80px;height:40px;"> or select change button <img src="change.png" alt="change" 
+        <p>4. Press enter <img src="images/enter.jpg" alt="enter" style="width:80px;height:40px;"> or select change button <img src="images/change.png" alt="change" 
               style="width:80px;height:40px;">.</p>
         <br>
         <p>5. Message "Your password has been changed" will appear confirming change password was successful.</p>
--- Views/v_search_man.php
+++ Views/v_search_man.php
@@ -1,7 +1,7 @@
 <?php
 include "v_tech_nav.php";
-
 ?>
+
 <div id="main-container">
     <div class="content" id="item1">
         <h1>SEARCH</h1> 
@@ -14,13 +14,13 @@ include "v_tech_nav.php";
         <br>
         <p> 1. Go to search box.</p>
         <br>        
-        <p><img src="search.png" alt="search_box"></p>       
+        <p><img src="images/search.png" alt="search_box"></p>       
         <br>    
         <p> 2. Enter the student ID or student name.</p>
         <br>
         <p>(Note: If using student ID, type # followed by student number. If using student name, type @ followed by student name.)</p>
         <br><br>
-        <p> 3. Press Enter  <img src="enter.jpg" alt="enter" style="width:80px;height:40px;"> or search tab button <img src="search_button.jpg" style="width:80px;height:40px;"></p> 
+        <p> 3. Press Enter  <img src="images/enter.jpg" alt="enter" style="width:80px;height:40px;"> or search tab button <img src="images/search_button.jpg" style="width:80px;height:40px;"></p> 
         <br>
     </div>
   
