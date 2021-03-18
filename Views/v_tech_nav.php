f6a1020f220c670bf703fc1ecc6469d64f831e9e
Minh Phan <trongminh20@gmail.com>
2021-03-18 12:24:46 -0700

    minh test

--- Views/v_tech_nav.php
+++ Views/v_tech_nav.php
@@ -5,10 +5,10 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/css_reset.css">
+    <link rel="stylesheet" href="css/css_reset.css">
 
     <title>Student Tracking System's Document</title>
-    <link rel="stylesheet" href="../css/technical_document.css">
+    <link rel="stylesheet" href="css/technical_document.css">
 </head>
 <body>
     <div id="body-wrapper">
@@ -18,11 +18,11 @@
         </div>
         <ul id="nav" class="nav-item">
             <li><a href="#introduction" class="nav-link">Introduction</a></li>
-            <li><a href="" class="nav-link"></a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
+            <li><a href="#resetPassword" class="nav-link"></a></li>
+            <li><a href="#item2" class="nav-link">sample</a></li>
+            <li><a href="#item3" class="nav-link">sample</a></li>
+            <li><a href="#item4" class="nav-link">sample</a></li>
+            <li><a href="#item5" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
