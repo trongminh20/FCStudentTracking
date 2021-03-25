e200d60782b16bcb84a738437e3463e50f5428dd
Minh Phan <trongminh20@gmail.com>
2021-03-25 00:55:52 -0700

    On branch minhphan  Changes to be committed: 	modified:   Views/v_home.php

--- Views/v_home.php
+++ Views/v_home.php
@@ -1,9 +1,9 @@
 <?php
-//include "v_masterPage_header.php";
-//?>
+include "v_masterPage_header.php";
+?>
 <?php
-//include "v_masterPage_sidebar.php";
-//?>
+include "v_masterPage_sidebar.php";
+?>
 
 <div class="container">
     <div id="mainContent">
