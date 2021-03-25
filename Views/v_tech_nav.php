aa65aa2c23b1781124ce0a9748e193faa36ec7e1
athomas97 <allythomas97@gmail.com>
2021-03-25 13:16:51 -0700

    On branch Ally  	modified:   Views/v_tech_nav.php

--- Views/v_tech_nav.php
+++ Views/v_tech_nav.php
@@ -5,10 +5,10 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/css_reset.css">
+    <link rel="stylesheet" href="css/css_reset.css">
 
     <title>Student Tracking System's Document</title>
-    <link rel="stylesheet" href="../css/technical_document.css">
+    <link rel="stylesheet" href="css/technical_document.css">
 </head>
 <body>
     <div id="body-wrapper">
@@ -18,8 +18,8 @@
         </div>
         <ul id="nav" class="nav-item">
             <li><a href="#introduction" class="nav-link">Introduction</a></li>
-            <li><a href="" class="nav-link"></a></li>
-            <li><a href="" class="nav-link">sample</a></li>
+            <li><a href="#ResetPass" class="nav-link"></a></li>
+            <li><a href="#GenerateInvoice" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
             <li><a href="" class="nav-link">sample</a></li>
