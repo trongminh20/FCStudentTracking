13361ad568f148f6cb347de40909498051cc6156
athomas97 <allythomas97@gmail.com>
2021-04-13 14:54:30 -0700

    On branch Ally  	modified:   v_tech_nav.php

--- Views/v_tech_nav.php
+++ Views/v_tech_nav.php
@@ -6,15 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/css_reset.css">
-<<<<<<< HEAD
-
     <title>Student Tracking System's Document</title>
     <link rel="stylesheet" href="css/technical_document.css">
-=======
-    <title>Student Tracking System's Document</title>
-    <link rel="stylesheet" href="css/technical_document.css">
-    <script src="js/customizeJS.js" type="text/javascript"></script>
->>>>>>> origin/demo
+    <script src="js/customizeJS.js" type="text/javascript"></script
 </head>
 <body>
 <div id="body-wrapper">
@@ -22,30 +16,19 @@
         <div id="page-title" class="nav-item">
             Student Tracking
         </div>
-        <ul id="nav" class="nav-item">
-            <li><a href="#introduction" class="nav-link">Introduction</a></li>
-            <li><a href="#ResetPass" class="nav-link"></a></li>
-            <li><a href="#GenerateInvoice" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
-            <li><a href="" class="nav-link">sample</a></li>
         <ul id="nav" class="nav-item" onclick="set_active_link();">
             <li class="nav_link"><a href="#introduction" class="nav-link">Introduction</a></li>
             <li class="nav_link"><a href="#resetPassword" class="nav-link">Reset Password</a></li>
             <li class="nav_link"><a href="#changePassword" class="nav-link">Change Password</a></li>
-            <li class="nav_link"><a href="#search" class="nav-link">How to search student</a></li>
-            <li class="nav_link"><a href="#addRecord" class="nav-link">Adding new Record</a></li>
-            <li class="nav_link"><a href="#updateRecord" class="nav-link">Updating record</a></li>
-            <li class="nav_link"><a href="#generateInvoice" class="nav-link">Generating new invoice</a></li>
-            <li class="nav_link"><a href="#generateReport" class="nav-link">Generating new invoice</a></li>
-            <li class="nav_link"><a href="#manageUserAddEmployee" class="nav-link">Generating new invoice</a></li>
-            <li class="nav_link"><a href="#manageUserRemoveEmployee" class="nav-link">Generating new invoice</a></li>
-            <li class="nav_link"><a href="#manageUserResetEmployee" class="nav-link">Generating new invoice</a></li>
-            <li class="nav_link"><a href="#manageUserEditEmployee" class="nav-link">Generating new invoice</a></li>
+            <li class="nav_link"><a href="#search" class="nav-link">Search for Students</a></li>
+            <li class="nav_link"><a href="#addRecord" class="nav-link">Add New Record</a></li>
+            <li class="nav_link"><a href="#updateRecord" class="nav-link">Update Record</a></li>
+            <li class="nav_link"><a href="#generateInvoice" class="nav-link">Generate New Invoice</a></li>
+            <li class="nav_link"><a href="#generateReport" class="nav-link">Generate New Report</a></li>
+            <li class="nav_link"><a href="#manageUserAddEmployee" class="nav-link">Add Employee</a></li>
+            <li class="nav_link"><a href="#manageUserRemoveEmployee" class="nav-link">Remove Employee</a></li>
+            <li class="nav_link"><a href="#manageUserResetEmployee" class="nav-link">Reset Employee</a></li>
+            <li class="nav_link"><a href="#manageUserEditEmployee" class="nav-link">Edit Employee</a></li>
             <li class="nav_link"><a href="?action=v_home" class="nav-link">Go To Dashboard</a></li>
         </ul>
     </nav>
